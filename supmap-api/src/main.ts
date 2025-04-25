16f16e4f5f691443b930e26a4426886da61a6f1e
Sanchez, Achille (ITDV VA) - AF <acsanchez1@airfrance.fr>
2025-04-26 01:08:53 +0200

    config for test

--- supmap-api/src/main.ts
+++ supmap-api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const port = Number(process.env.PORT!) || '3000'; // Default to port 3000 if undefined
+  await app.listen(port);
 }
 bootstrap();
