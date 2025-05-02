c35e426da8ffa06d106380f6359048da1ad9577c
Sanchez, Achille (ITDV VA) - AF <acsanchez1@airfrance.fr>
2025-05-02 19:51:36 +0200

    Update roles.guard.ts

--- supmap-api/src/auth/guards/roles/roles.guard.ts
+++ supmap-api/src/auth/guards/roles/roles.guard.ts
@@ -1,11 +1,40 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../../decorators/roles.decorator/roles.decorator';
+import { UserRole } from '../../../users/user.entity/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<UserRole[]>(
+      ROLES_KEY,
+      ctx.getHandler(),
+    );
+    // Si aucune restriction, on autorise
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const { user } = ctx.switchToHttp().getRequest();
+    if (!user) {
+      throw new ForbiddenException('Utilisateur non authentifié');
+    }
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        `Accès refusé : rôles autorisés [${requiredRoles.join(
+          ', ',
+        )}], rôle actuel : ${user.role}`,
+      );
+    }
+
     return true;
   }
 }
