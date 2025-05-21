c1774f923ccc451af75d277c77e344c03ad490fc
Sanchez, Achille (ITDV VA) - AF <acsanchez1@airfrance.fr>
2025-05-21 16:49:21 +0200

    Update main.ts

--- supmap-api/src/main.ts
+++ supmap-api/src/main.ts
@@ -1,9 +1,11 @@
+
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
